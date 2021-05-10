98c5109a30add613a1cbb79cf3e6ce8ab104d9c8
raosan <raosanf.lillahi@gmail.com>
2021-05-10 12:48:31 +0700

    use cause file

--- ios/causeTests/causeTests.m
+++ ios/causeTests/causeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAppTests : XCTestCase
+@interface causeTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation causeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
